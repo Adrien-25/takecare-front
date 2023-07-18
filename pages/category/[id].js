1bf200b9d8b973213d0849ef879e878a132b008d
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-07-18 16:49:49 +0200

    Ajout des page catégories afficher avec leur id

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -1,3 +1,6 @@
+
+
+
 import Header from "@/components/Header";
 import styled from "styled-components";
 import Center from "@/components/Center";
@@ -6,29 +9,27 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 import ProductsGrid from "@/components/ProductsGrid";
 import Title from "@/components/Title";
+import {useContext} from "react";
+import {CartContext} from "@/components/CartContext";
 
 export default function CategoriesPage({ products }) {
-  const router = useRouter();
-  const { categoryId } = router.query;
-
-  const filteredProducts = categoryId
-    ? products.filter((product) => product.category === categoryId)
-    : products;
 
   return (
     <>
       <Header />
       <Center>
         <Title>All products</Title>
-        <ProductsGrid products={filteredProducts} />
+        <ProductsGrid products={products} />
       </Center>
     </>
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
   await mongooseConnect();
-  const products = await Product.find({}, null, { sort: { '_id': -1 } });
+  const {id} = context.query;
+  const products = await Product.find({ "category": id }, null, { sort: { '_id': -1 } });
+
   return {
     props: {
       products: JSON.parse(JSON.stringify(products)),
